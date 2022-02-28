b0bf76f1607c5b3f16464cf472d3bcc0883fa514
Srknkrt <srkn41krt@gmail.com>
2022-02-28 18:59:04 +0300

    Version Finish

---
+++ Periyodik Tablo/main.c
@@ -0,0 +1,251 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   int x;
+    printf("Atom numarasi giriniz:");
+    scanf("%d",&x);
+
+    if (x==1)
+    printf("Hidrojen");
+    else if (x==2)
+    printf("Helyum");
+    else if (x==3)
+    printf("Lityum");
+    else if (x==4)
+    printf("Berilyum");
+    else if (x==5)
+    printf("Bor");
+    else if (x==6)
+    printf("Karbon");
+    else if (x==7)
+    printf("Azot");
+    else if (x==8)
+    printf("Oksijen");
+    else if (x==9)
+    printf("Flor");
+    else if (x==10)
+    printf("Neon");
+    else if (x==11)
+    printf("Sodyum");
+    else if (x==12)
+    printf("Magnezyum");
+    else if (x==13)
+    printf("Aluminyum");
+    else if (x==14)
+    printf("Silisyum");
+    else if (x==15)
+    printf("Fosfor");
+    else if (x==16)
+    printf("Kukurt");
+    else if (x==17)
+    printf("Klor");
+    else if (x==18)
+    printf("Argon");
+    else if (x==19)
+    printf("Potasyum");
+    else if (x==20)
+    printf("Kalsiyum");
+    else if (x==21)
+    printf("Skandiyum");
+    else if (x==22)
+    printf("Titanyum");
+    else if (x==23)
+    printf("Vanadyum");
+    else if (x==24)
+    printf("Krom");
+    else if (x==25)
+    printf("Mangan");
+    else if (x==26)
+    printf("Demir");
+    else if (x==27)
+    printf("Kobalt");
+    else if (x==28)
+    printf("Nikel");
+    else if (x==29)
+    printf("Bakir");
+    else if (x==30)
+    printf("Cinko");
+    else if (x==31)
+    printf("Galyum");
+    else if (x==32)
+    printf("Germanyum");
+    else if (x==33)
+    printf("Arsenik");
+    else if (x==34)
+    printf("Selenyum");
+    else if (x==35)
+    printf("Brom");
+    else if (x==36)
+    printf("Kripton");
+    else if (x==37)
+    printf("Rubidyum");
+    else if (x==38)
+    printf("Strontiyum");
+    else if (x==39)
+    printf("Itriyum");
+    else if (x==40)
+    printf("Zirkon");
+    else if (x==41)
+    printf("Niobyum");
+    else if (x==42)
+    printf("Molibden");
+    else if (x==43)
+    printf("Tekhnetyum");
+    else if (x==44)
+    printf("Rutenyum");
+    else if (x==45)
+    printf("Rodyum");
+    else if (x==46)
+    printf("Palladyum");
+    else if (x==47)
+    printf("Gumus");
+    else if (x==48)
+    printf("Kadminyum");
+    else if (x==49)
+    printf("Indiyum");
+    else if (x==50)
+    printf("Kalay");
+    else if (x==51)
+    printf("Antimmon");
+    else if (x==52)
+    printf("Tellur");
+    else if (x==53)
+    printf("Iyot");
+    else if (x==54)
+    printf("Ksenon");
+    else if (x==55)
+    printf("Sezyum");
+    else if (x==56)
+    printf("Baryum");
+    else if (x==57)
+    printf("Lantan");
+    else if (x==58)
+    printf("Seryum");
+    else if (x==59)
+    printf("Praseodim");
+    else if (x==60)
+    printf("Neodim");
+    else if (x==61)
+    printf("Prometyum");
+    else if (x==62)
+    printf("Samaryum");
+    else if (x==63)
+    printf("Evropyum");
+    else if (x==64)
+    printf("Gadolinyum");
+    else if (x==65)
+    printf("Terbiyum");
+    else if (x==66)
+    printf("Disprosiyum");
+    else if (x==67)
+    printf("Holmiyum");
+    else if (x==68)
+    printf("Erbiyum");
+    else if (x==69)
+    printf("Tulyum");
+    else if (x==70)
+    printf("Itterbiyum");
+    else if (x==71)
+    printf("Lutetyum");
+    else if (x==72)
+    printf("Hafniyum");
+    else if (x==73)
+    printf("Tantal");
+    else if (x==74)
+    printf("Tungsten");
+    else if (x==75)
+    printf("Renyum");
+    else if (x==76)
+    printf("Osminyum");
+    else if (x==77)
+    printf("Iridyum");
+    else if (x==78)
+    printf("Platin");
+    else if (x==79)
+    printf("Altin");
+    else if (x==80)
+    printf("Civa");
+    else if (x==81)
+    printf("Talyum");
+    else if (x==82)
+    printf("Kursun");
+    else if (x==83)
+    printf("Bizmut");
+    else if (x==84)
+    printf("Polonyum");
+    else if (x==85)
+    printf("Astatin");
+    else if (x==86)
+    printf("Radon");
+    else if (x==87)
+    printf("Fransiyum");
+    else if (x==88)
+    printf("Radyum");
+    else if (x==89)
+    printf("Aktinyum");
+    else if (x==90)
+    printf("Toryum");
+    else if (x==91)
+    printf("Protaktinyum");
+    else if (x==92)
+    printf("Uranyum");
+    else if (x==93)
+    printf("Neptunyum");
+    else if (x==94)
+    printf("Plutonyum");
+    else if (x==95)
+    printf("Amerikyum");
+    else if (x==96)
+    printf("Kuriyum");
+    else if (x==97)
+    printf("Berkelyum");
+    else if (x==98)
+    printf("Kaliforniyum");
+    else if (x==99)
+    printf("Einsteinium");
+    else if (x==100)
+    printf("Fermiyum");
+    else if (x==101)
+    printf("Mendelevyum");
+    else if (x==102)
+    printf("Nobelyum");
+    else if (x==103)
+    printf("Lavrensiyum");
+    else if (x==104)
+    printf("Rutherfordiyum");
+    else if (x==105)
+    printf("Dubniyum");
+    else if (x==106)
+    printf("Seaborgiyum");
+    else if (x==107)
+    printf("Bohriyum");
+    else if (x==108)
+    printf("Hassiyum");
+    else if (x==109)
+    printf("Meitneriyum");
+    else if (x==110)
+    printf("Darmstadtiyum");
+    else if (x==111)
+    printf("Ununnilyum");
+    else if (x==112)
+    printf("Ununbiyum");
+    else if (x==113)
+    printf("Ununtriyum");
+    else if (x==114)
+    printf("Ununkuadyum");
+    else if (x==115)
+    printf("Ununpentiyum");
+    else if (x==116)
+    printf("Ununheksiyum");
+    else if (x==117)
+    printf("Ununseptiyum");
+    else if (x==118)
+    printf("Ununoktiyum");
+
+
+    else
+    printf("Lutfen sadece 1-118 arasi sayilar giriniz.");
+
+    return 0;
+}
